ead299255528615514a3d57cb3acbc13ef428e32
Bibash Basnet <basnetbibash32@gmail.com>
2024-05-07 21:06:33 +0545

    React project for college

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Nav_bar from "./nav-bar";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav_bar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/nav-bar.js
@@ -0,0 +1,26 @@
+function Nav_bar() {
+    return (
+      <div className="nav_bar">
+        <div className="logo">
+            <ul>
+              <li><img src="" alt="" className=""></img></li>
+              <li>Ecommerce</li>
+            </ul>
+        </div>
+        <div className="nav_content">
+          <ul>
+              <li>Home</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>Explore All</li>
+            </ul>
+        </div>
+        <div className="cart">
+          
+        </div>
+      </div>
+    );
+  }
+  
+  export default Nav_bar;
+  
